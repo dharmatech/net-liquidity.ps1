9823fbff227698f4342b1ea5007096f9aeb60b0c
dharmatech <wayo.cavazos@gmail.com>
2023-04-27 14:50:12 -0700

    Updates

--- net-liquidity-wshosho.ps1
+++ net-liquidity-wshosho.ps1
@@ -212,139 +212,139 @@ Write-Host 'DATE                    WSHOSHO              CHANGE                 
 # Page template
 # ----------------------------------------------------------------------
 
-$page_template = @"
-<!doctype html>
-<html lang="en">
+# $page_template = @"
+# <!doctype html>
+# <html lang="en">
 
-<head>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.4/dist/bootstrap-table.min.css">
+# <head>
+#   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+#     integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+#   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+#   <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.21.4/dist/bootstrap-table.min.css">
 
-</head>
+# </head>
 
-</head>
+# </head>
 
-<body>
+# <body>
 
-  <div class="container-fluid">
-    <div class="row flex-nowrap">
-      <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 ">
-        <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-          <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-            <span class="fs-5 d-none d-sm-inline">Menu</span>
-          </a>
-          <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+#   <div class="container-fluid">
+#     <div class="row flex-nowrap">
+#       <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 ">
+#         <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+#           <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+#             <span class="fs-5 d-none d-sm-inline">Menu</span>
+#           </a>
+#           <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
             
-            <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0">
-                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Net Liquidity Table (WSHOSHO)</span>
-              </a>
-            </li>
+#             <li class="nav-item">
+#               <a href="#" class="nav-link align-middle px-0">
+#                 <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Net Liquidity Table (WSHOSHO)</span>
+#               </a>
+#             </li>
 
-            <li class="nav-item">
-              <a href="#" class="nav-link align-middle px-0">
-                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Net Liquidity Chart (WSHOSHO)</span>
-              </a>
-            </li>
+#             <li class="nav-item">
+#               <a href="#" class="nav-link align-middle px-0">
+#                 <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Net Liquidity Chart (WSHOSHO)</span>
+#               </a>
+#             </li>
 
 
 
-            <li>
-              <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span> </a>
-              <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                <li class="w-100">
-                  <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a>
-                </li>
-                <li>
-                  <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
-            </li>
-            <li>
-              <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
-                <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Bootstrap</span></a>
-              <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                <li class="w-100">
-                  <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
-                </li>
-                <li>
-                  <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
-              <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                <li class="w-100">
-                  <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
-                </li>
-                <li>
-                  <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
-                </li>
-                <li>
-                  <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
-                </li>
-                <li>
-                  <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#" class="nav-link px-0 align-middle">
-                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
-            </li>
-          </ul>
-          <hr>
-          <div class="dropdown pb-4">
-            <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-              id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-              <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
-              <span class="d-none d-sm-inline mx-1">menu</span>
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-              <li><a class="dropdown-item" href="#">New project...</a></li>
-              <li><a class="dropdown-item" href="#">Settings</a></li>
-              <li><a class="dropdown-item" href="#">Profile</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Sign out</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
+#             <li>
+#               <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+#                 <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span> </a>
+#               <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+#                 <li class="w-100">
+#                   <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1 </a>
+#                 </li>
+#                 <li>
+#                   <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2 </a>
+#                 </li>
+#               </ul>
+#             </li>
+#             <li>
+#               <a href="#" class="nav-link px-0 align-middle">
+#                 <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
+#             </li>
+#             <li>
+#               <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
+#                 <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Bootstrap</span></a>
+#               <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
+#                 <li class="w-100">
+#                   <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
+#                 </li>
+#                 <li>
+#                   <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
+#                 </li>
+#               </ul>
+#             </li>
+#             <li>
+#               <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+#                 <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
+#               <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
+#                 <li class="w-100">
+#                   <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
+#                 </li>
+#                 <li>
+#                   <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
+#                 </li>
+#                 <li>
+#                   <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
+#                 </li>
+#                 <li>
+#                   <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
+#                 </li>
+#               </ul>
+#             </li>
+#             <li>
+#               <a href="#" class="nav-link px-0 align-middle">
+#                 <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
+#             </li>
+#           </ul>
+#           <hr>
+#           <div class="dropdown pb-4">
+#             <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+#               id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+#               <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
+#               <span class="d-none d-sm-inline mx-1">menu</span>
+#             </a>
+#             <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+#               <li><a class="dropdown-item" href="#">New project...</a></li>
+#               <li><a class="dropdown-item" href="#">Settings</a></li>
+#               <li><a class="dropdown-item" href="#">Profile</a></li>
+#               <li>
+#                 <hr class="dropdown-divider">
+#               </li>
+#               <li><a class="dropdown-item" href="#">Sign out</a></li>
+#             </ul>
+#           </div>
+#         </div>
+#       </div>
 
-      <div class="col py-3">
+#       <div class="col py-3">
 
-        {0}
+#         {0}
 
-      </div>
-    </div>
-  </div>
+#       </div>
+#     </div>
+#   </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-    crossorigin="anonymous"></script>
-  <script src="https://unpkg.com/bootstrap-table@1.21.4/dist/bootstrap-table.min.js"></script>
+#   <script src="https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js"></script>
+#   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+#     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+#     crossorigin="anonymous"></script>
+#   <script src="https://unpkg.com/bootstrap-table@1.21.4/dist/bootstrap-table.min.js"></script>
 
-  {1}
-  {2}
-</body>
-"@
+#   {1}
+#   {2}
+# </body>
+# "@
 
-function page-template ($a, $b, $c)
-{
-    $page_template -f $a, $b, $c
-}
+# function page-template ($a, $b, $c)
+# {
+#     $page_template -f $a, $b, $c
+# }
 
 # ----------------------------------------------------------------------
 # HTML table
@@ -378,7 +378,7 @@ function html-td ($val, $class)
 
 $file = 'net-liquidity-wshosho-table-partial.html'
 
-'<table class="table table-sm" data-toggle="table" data-height="500">' > $file
+'<table class="table table-sm" data-toggle="table" data-height="800">' > $file
 
 '<thead>' >> $file
 '<tr>' >> $file
